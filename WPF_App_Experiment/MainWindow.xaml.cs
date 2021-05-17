ee213a4fccf0833286a2394daab9d70eff29fc5b
Mukund-Iyer <82875381+Mukund-Iyer@users.noreply.github.com>
2021-05-18 02:44:07 +0530

    Add project files.

---
+++ WPF_App_Experiment/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+using System.Xml;
+
+namespace WPF_App_Experiment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void On_Load(object sender, RoutedEventArgs e)
+        {
+            string url = "https://www.thehindu.com/business/markets/feeder/default.rss";
+            XmlDocument XDoc = new XmlDocument();
+            XDoc.Load(url);
+            XmlNodeList XMLNodes;
+            XMLNodes = XDoc.ChildNodes[1].ChildNodes[0].ChildNodes;
+            foreach(XmlNode Node in XMLNodes)
+            {
+                if(Node.Name=="item")
+                {
+                    string test_1 = Node.ChildNodes[0].InnerText;
+                    string test_2 = Node.ChildNodes[4].InnerText;
+                    string test_3 = Node.ChildNodes[5].ChildNodes[0].Value;
+                    News_Bubbles uc1 = new News_Bubbles();
+                    uc1.Heading_Box_Text = test_1.Trim();
+                    uc1.Content_Box_Text = test_2.Trim();
+                    uc1.Timestamp_Box_Text = test_3.Trim();
+                    List_Format.Items.Add(uc1);
+                    //Feed_Box.Items.Add(test);
+                }
+            }
+            
+        }
+
+        private void UserControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+            //List_Format.Items.Add();
+        }
+
+        private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Exit_Button_Action(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MouseDown_Event(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+ }
---
+++ WPF_App_Experiment/News_Bubbles.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_App_Experiment
+{
+    /// <summary>
+    /// Interaction logic for News_Bubbles.xaml
+    /// </summary>
+    public partial class News_Bubbles : UserControl
+    {
+        public News_Bubbles()
+        {
+            InitializeComponent();
+        }
+        public string Heading_Box_Text
+        {
+            get { return Heading_Box.Text; }
+            set { Heading_Box.Text = value; }
+        }
+        public string Content_Box_Text
+        {
+            get { return Content_Box.Text; }
+            set { Content_Box.Text = value; }
+        }
+        public string Timestamp_Box_Text
+        {
+            get { return Timestamp_Box.Text; }
+            set { Timestamp_Box.Text = value; }
+        }
+    }
+}
