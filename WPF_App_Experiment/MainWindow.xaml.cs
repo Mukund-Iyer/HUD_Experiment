08c0e5a5378eb9f4ffb770f74dc3bdf788e7b583
Mukund-Iyer <82875381+Mukund-Iyer@users.noreply.github.com>
2023-11-20 14:26:10 +0530

    Major Updates
    (1) Modified the RSS Reading algorithm to read multiple topics under Business and allow user to load any of the topics under it.
    (2) Images are now loaded as well as the text with their headings.

--- WPF_App_Experiment/MainWindow.xaml.cs
+++ WPF_App_Experiment/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,14 @@ namespace WPF_App_Experiment
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string MarketURL = "https://www.thehindu.com/business/markets/feeder/default.rss";
+        private string BusinesslURL = "https://www.thehindu.com/business/feeder/default.rss";
+        private string AgriBusinessURL = "https://www.thehindu.com/business/agri-business/feeder/default.rss";
+        private string EconomyURL = "https://www.thehindu.com/business/Economy/feeder/default.rss";
+        private string IndustryURL = "https://www.thehindu.com/business/Industry/feeder/default.rss";
+        private string BudgetURL = "https://www.thehindu.com/business/budget/feeder/default.rss";
+        private string DefaultURL = "https://www.thehindu.com/business/feeder/default.rss";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,29 +41,113 @@ namespace WPF_App_Experiment
 
         private void On_Load(object sender, RoutedEventArgs e)
         {
-            string url = "https://www.thehindu.com/business/markets/feeder/default.rss";
-            XmlDocument XDoc = new XmlDocument();
-            XDoc.Load(url);
-            XmlNodeList XMLNodes;
-            XMLNodes = XDoc.ChildNodes[1].ChildNodes[0].ChildNodes;
-            foreach(XmlNode Node in XMLNodes)
-            {
-                if(Node.Name=="item")
-                {
-                    string test_1 = Node.ChildNodes[0].InnerText;
-                    string test_2 = Node.ChildNodes[4].InnerText;
-                    string test_3 = Node.ChildNodes[5].ChildNodes[0].Value;
-                    News_Bubbles uc1 = new News_Bubbles();
-                    uc1.Heading_Box_Text = test_1.Trim();
-                    uc1.Content_Box_Text = test_2.Trim();
-                    uc1.Timestamp_Box_Text = test_3.Trim();
-                    List_Format.Items.Add(uc1);
-                    //Feed_Box.Items.Add(test);
-                }
-            }
-            
         }
 
+        private async Task LoadData(string Option)
+        {
+            List_Format.Items.Clear();
+            string url = GetUrlForOption(Option);
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string xmlData = await client.GetStringAsync(url);
+                    XmlDocument XDoc = new XmlDocument();
+                    XDoc.LoadXml(xmlData);
+
+                    XmlNodeList XMLNodes = XDoc.SelectNodes("/rss/channel/item");
+
+                    if (XMLNodes != null)
+                    {
+                        List<Task> downloadTasks = new List<Task>();
+
+                        foreach (XmlNode Node in XMLNodes)
+                        {
+                            News_Bubbles uc1 = new News_Bubbles();
+
+                            foreach (XmlNode Itex in Node.ChildNodes)
+                            {
+                                switch (Itex.Name)
+                                {
+                                    case "title":
+                                        uc1.Heading_Box_Text = Itex.InnerText;
+                                        break;
+                                    case "description":
+                                        uc1.Content_Box_Text = Itex.InnerText;
+                                        break;
+                                    case "pubDate":
+                                        uc1.Timestamp_Box_Text = Itex.InnerText;
+                                        break;
+                                    case "media:content":
+                                        downloadTasks.Add(DownloadImageAsync(Itex, uc1));
+                                        break;
+                                }
+                            }
+
+                            if (!string.IsNullOrEmpty(uc1.Heading_Box_Text) && !string.IsNullOrEmpty(uc1.Content_Box_Text) && !string.IsNullOrEmpty(uc1.Timestamp_Box_Text))
+                            {
+                                List_Format.Items.Add(uc1);
+                            }
+                        }
+
+                        await Task.WhenAll(downloadTasks);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+            }
+        }
+
+        private async Task DownloadImageAsync(XmlNode mediaContentNode, News_Bubbles uc1)
+        {
+            string ImageLink = mediaContentNode.Attributes["url"].Value;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    byte[] data = await client.GetByteArrayAsync(ImageLink);
+
+                    using (MemoryStream stream = new MemoryStream(data))
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.StreamSource = stream;
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.EndInit();
+
+                        uc1.Content_Image.Source = bitmapImage;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle image download exceptions
+            }
+        }
+
+        private string GetUrlForOption(string option)
+        {
+            switch (option)
+            {
+                case "Markets":
+                    return MarketURL;
+                case "Business":
+                    return BusinesslURL;
+                case "Economy":
+                    return EconomyURL;
+                case "Industry":
+                    return IndustryURL;
+                case "Agri-Business":
+                    return AgriBusinessURL;
+                default:
+                    return DefaultURL;
+            }
+        }
+        
         private void UserControl1_Loaded(object sender, RoutedEventArgs e)
         {
             //List_Format.Items.Add();
@@ -71,6 +166,22 @@ namespace WPF_App_Experiment
         private void MouseDown_Event(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void TransparencySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MainGrid.Opacity = TransparencySlider.Value;
+        }
+
+        private void RefreshButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            LoadData(Top_Label.SelectedValue.ToString());
+        }
+
+        private void Top_Label_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            System.Windows.Controls.ComboBox x = (System.Windows.Controls.ComboBox)sender;
+            LoadData(Top_Label.SelectedValue.ToString());
         }
     }
  }
